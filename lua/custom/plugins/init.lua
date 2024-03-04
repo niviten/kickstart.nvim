72d8623fb0bea665fc39102bb1f38e16ac7e51b3
niviten <l.j.nivethan@gmail.com>
2024-03-04 08:04:04 +0530

    Remove unwanted comments

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -9,12 +9,6 @@ return {
     branch = 'harpoon2',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-      -- local mark = require 'harpoon.mark'
-      -- local ui = require 'harpoon.ui'
-      --
-      -- vim.keymap.set('n', '<leader>a', mark.add_file)
-      -- vim.keymap.set('n', '<leader>e', ui.toggle_quick_menu)
-
       local harpoon = require("harpoon")
 
       harpoon:setup()
